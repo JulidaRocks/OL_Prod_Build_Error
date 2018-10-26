6ed6ca7afcf50352167ce63ec33f803bd4a07f57
Mira Manger <mira.manger@gmx.de>
2018-10-26 19:53:07 +0200

    created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import OlTileLayer from 'ol/layer/Tile';
+import OlMap from 'ol/Map';
+import OlView from 'ol/View';
+import OlOsmSource from 'ol/source/OSM';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'test-ol';
+
+  ngOnInit() {
+    this.initOpenLayerMap();
+  }
+
+  initOpenLayerMap() {
+    const olview = new OlView({ center: [0, 0], zoom: 2 }),
+      baseLayer = new OlTileLayer({ source: new OlOsmSource() }),
+      map = new OlMap({
+        target: document.getElementById('map'),
+        view: olview,
+        layers: [baseLayer]
+      });
+  }
 }
